780c7e4175ca5a1c6ecb11079219d309489f34a7
kim_ki_yun <kiyoon9390@gmail.com>
2022-02-02 23:59:20 +0900

    [FEAT] order entity DTO

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication // 하위에 있는 모는 component들을 관리
 public class JpashopApplication {
@@ -9,5 +11,11 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	} // DTO로 client에 노출할 것이기 때문에 사실 크게 쓸모가 없다 -> member : null / orderItems : null이 된다
+	// 지연로딩 되게 하는 것은 모두 null로 표시됨
 
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,81 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * X To One 관계에서의 성능 최적화
+ * Order
+ * Order -> Member (M : 1)
+ * Order -> Delivery (1 : 1)
+ * Order -> OrderItem (1 : N)
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+
+        for (Order order : all){
+            order.getMember().getName();
+            // getMember까지는 프록시 객체, getName 하는 순간부터 쿼리가 날라감 - LAZY 강제 초기화
+            order.getDelivery().getAddress();
+        }
+        // 그렇다고 fetch type을 EAGER로 바꾸면
+        // em.find()는 JPA가 알아서 성능 최적화된 쿼리를 보내주지만, 직접 만든 메소드 같은 경우 JPQL이 날라감 -> 그대로 SQL로 변환 (Order만 가져오고 가져오고 보니 Member를 안가져와서 단건으로 조회가 나감 N+1문제)
+        // -> 다른 API가 필요없다하더라도 가져옴
+        return all;
+    }
+    // [문제1] 무한루프에 빠짐 - member로 가고 order로 가고 member로 가고 ..... @JSONIGNORE로 끊어주어도
+    // [문제2] ByteBuddyInterceptor - fetchType LAZY로 db에서 가져오지 않고 member = new ProxyMember() (프록시 객체) (ByteBuddyInterceptor())를 임의로 넣어주는데 그때 쓰는 프록시 기술이 bytebuddy
+    //-> Hibernate5 module을 등록해야
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+//        List<SimpleOrderDto> result = orders.stream()
+//                .map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+        List<SimpleOrderDto> result =orderRepository.findAll(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+        // lambda 를 적용한 경
+        return result;
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
+}
